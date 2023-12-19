bd7f5a92d9bd24dc503a0c9033bf914553a19105
JoelALukwe <alukwe7@gmail.com>
2023-12-19 11:58:37 -0800

    0. My first square

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
 
-"""Define a class Square."""
+"""Defination of a class square
+
+it is a plane figure with four equal straight sides and four right angles.
+"a grid of ruled squares"."""
 
 
 class Square:
