531bead89b6c57c80e6c57f2ee8ea6a587ed5ab2
JoelALukwe <alukwe7@gmail.com>
2024-03-22 16:42:01 -0700

    17. From city

--- 0x0F-python-object_relational_mapping/102-relationship_cities_states_list.py
+++ 0x0F-python-object_relational_mapping/102-relationship_cities_states_list.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
-""" prints the State object with the name passed as argument from the database
-"""
+""" this code block will list all City objects from the database hbtn_0e_101_usa """
 import sys
 from relationship_state import Base, State
 from relationship_city import City
