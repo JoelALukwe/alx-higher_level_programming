c494194bafc44c428cb632a07ea24dad36aa17f0
JoelALukwe <alukwe7@gmail.com>
2024-03-22 17:04:47 -0700

    17. From city

--- 0x0F-python-object_relational_mapping/102-relationship_cities_states_list.py
+++ 0x0F-python-object_relational_mapping/102-relationship_cities_states_list.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-""" this code block will list all City objects from the database hbtn_0e_101_usa """
+""" prints the State object with the name passed as argument from the database
+"""
 import sys
 from relationship_state import Base, State
 from relationship_city import City
@@ -9,12 +10,13 @@ from sqlalchemy.orm import relationship
 
 
 if __name__ == "__main__":
-engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
-.format(sys.argv[1], sys.argv[2], sys.argv[3]))
-Base.metadata.create_all(engine)
-Session = sessionmaker(bind=engine)
-session = Session()
-for instance in session.query(State).order_by(State.id):
-for city_ins in instance.cities:
-print(city_ins.id, city_ins.name, sep=": ", end="")
-print(" -> " + instance.name)
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]))
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    for instance in session.query(State).order_by(State.id):
+        print(instance.id, instance.name, sep=": ")
+        for city_ins in instance.cities:
+            print("    ", end="")
+            print(city_ins.id, city_ins.name, sep=": ")
